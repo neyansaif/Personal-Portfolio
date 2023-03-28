e289c377d750f2a3edf0fba77118f317ad887ce5
neyansaif <neyan7163529@gmail.com>
2023-03-29 02:17:45 +0500

    All filies once

---
+++ src/components/About.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div
+      name="about"
+      className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white pt-24"
+    >
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+            About
+          </p>
+        </div>
+
+        <p className="pt-10">
+          Hello, my name is Neyan Saif and I am a software engineer with 2 years
+          of experience in web development. Throughout my career, I have gained
+          expertise in a variety of technologies, including HTML, CSS,
+          Bootstrap, Tailwind, Material UI, React, Node.js, Express, MongoDB,
+          and Mongoose.
+        </p>
+        <br />
+        <p>
+          As a developer, I am passionate about creating clean and efficient
+          code that delivers a seamless user experience. I am a problem-solver
+          at heart and enjoy taking on challenging projects that push me to
+          learn and grow as a developer. I am also committed to staying
+          up-to-date with the latest trends and best practices in web
+          development, and am always seeking new ways to improve my skills and
+          knowledge.
+        </p>
+        <br />
+        <p>
+          In my free time, I enjoy contributing to open source projects and
+          participating in hackathons and coding challenges. I believe that the
+          best way to become a better developer is to continuously challenge
+          yourself and seek out new opportunities to learn and grow. If you're
+          looking for a skilled and motivated software engineer with a passion
+          for web development, please don't hesitate to get in touch!
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/components/Home.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import MyPic from "../Images/mypic.jpg";
+import { MdOutlineKeyboardArrowRight } from "react-icons/md";
+import { Link } from "react-scroll";
+
+const Home = () => {
+  return (
+    <div
+      name="home"
+      className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800"
+    >
+      <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
+        <div className="flex flex-col justify-center h-full">
+          <h2 className="text-4xl sm:text-7xl font-bold text-white pt-24">
+            I'm a Software Engineer
+          </h2>
+          <p className="text-gray-500 py-4 max-w-md">
+            Hello, my name is Neyan Saif and I am a software engineer with 2
+            years of experience in web development. Throughout my career, I have
+            gained expertise in a variety of technologies, including HTML, CSS,
+            Bootstrap, Tailwind, Material UI, React, Node.js, Express, MongoDB,
+            and Mongoose.
+          </p>
+          <div>
+            <Link
+              to="portfolio"
+              smooth
+              duration={500}
+              className="group text-white w-fit font-bold py-3 px-6 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer"
+            >
+              Portfolio
+              <span className="group-hover:rotate-90 duration-300">
+                <MdOutlineKeyboardArrowRight size={25} className="ml-1" />
+              </span>
+            </Link>
+          </div>
+        </div>
+        <div className="rounded">
+          <img
+            src={MyPic}
+            alt="My Profile pic"
+            width="500wh"
+            className="rounded-2xl mx-auto w2/3"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
