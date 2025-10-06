7e4cf9c3bdaa3361922924e7b6c0ce5ecd54520d
Neyan Saif <121193638+neyansaif@users.noreply.github.com>
2025-10-06 13:58:09 +0300

    - Favicon Change - Apply Routes

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Experience from "./components/Experience";
@@ -5,19 +6,22 @@ import Home from "./components/Home";
 import NavBar from "./components/NavBar";
 import Portfolio from "./components/Portfolio";
 import SocialLinks from "./components/SocialLinks";
+import Blog from "./components/Blog";
 
 const App = () => {
   return (
-    <div>
+    <Router>
       <NavBar />
-      <Home />
-      <About />
-      <Portfolio />
-      <Experience />
-      <Contact />
-      
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/experience" element={<Experience />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
       <SocialLinks />
-    </div>
+    </Router>
   );
 };
 
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,31 +1,17 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
-import { Link } from "react-scroll";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   const [nav, setnav] = useState(false);
 
   const Links = [
-    {
-      id: 1,
-      link: "home",
-    },
-    {
-      id: 2,
-      link: "about",
-    },
-    {
-      id: 3,
-      link: "portfolio",
-    },
-    {
-      id: 4,
-      link: "experience",
-    },
-    {
-      id: 5,
-      link: "contact",
-    },
+    { id: 1, link: "/" },
+    { id: 2, link: "/about" },
+    { id: 3, link: "/portfolio" },
+    { id: 4, link: "/experience" },
+    { id: 5, link: "/blog" },
+    { id: 6, link: "/contact" },
   ];
 
   return (
@@ -33,20 +19,18 @@ const NavBar = () => {
       <div>
         <h1 className="text-5xl font-signature ml-2">Neyan_saif</h1>
       </div>
+
       <ul className="hidden md:flex">
-        {Links.map(({ id, link }) => {
-          return (
-            <li
-              key={id}
-              className="px-4 cursor-pointer capitalize font-medium text-gray-400 hover:scale-105 duration-200"
-            >
-              <Link to={link} smooth duration={500}>
-                {link}
-              </Link>
-            </li>
-          );
-        })}
+        {Links.map(({ id, link }) => (
+          <li
+            key={id}
+            className="px-4 cursor-pointer capitalize font-medium text-gray-400 hover:scale-105 duration-200"
+          >
+            <Link to={link}>{link === "/" ? "home" : link.slice(1)}</Link>
+          </li>
+        ))}
       </ul>
+
       <div
         onClick={() => setnav(!nav)}
         className="cursor-pointer pr-4 z-10 text-gray-500 md:hidden"
@@ -56,23 +40,16 @@ const NavBar = () => {
 
       {nav && (
         <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500 md:hidden">
-          {Links.map(({ id, link }) => {
-            return (
-              <li
-                key={id}
-                className="px-4 cursor-pointer capitalize py-6 text-4xl"
-              >
-                <Link
-                  onClick={() => setnav(!nav)}
-                  to={link}
-                  smooth
-                  duration={500}
-                >
-                  {link}
-                </Link>
-              </li>
-            );
-          })}
+          {Links.map(({ id, link }) => (
+            <li
+              key={id}
+              className="px-4 cursor-pointer capitalize py-6 text-4xl"
+            >
+              <Link onClick={() => setnav(!nav)} to={link}>
+                {link === "/" ? "home" : link.slice(1)}
+              </Link>
+            </li>
+          ))}
         </ul>
       )}
     </div>
